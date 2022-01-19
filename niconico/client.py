ef3f7542d21086240f4fee4dcbe156a2138d78a5
tnt <45692264+tuna2134@users.noreply.github.com>
2022-01-19 17:16:39 +0900

    Add: requests.Session

--- niconico/client.py
+++ niconico/client.py
@@ -12,10 +12,11 @@ class NicoNico:
     def __init__(self, cookies: Optional[Cookies] = None):
         self.video = VideoClient()
         self.cookies = cookies
+        self.session = requests.Session()
 
     def request(self, *args, **kwargs) -> requests.Response:
         kwargs["cookies"] = self.cookies
-        response = requests.request(*args, **kwargs)
+        response = self.session.request(*args, **kwargs)
         if self.cookies is None:
             self.cookies = Cookies.guest(response.cookies["nicosid"])
         return response
